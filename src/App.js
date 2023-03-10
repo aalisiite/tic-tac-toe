9991b5c4ff3a291ed19fabb72f03ddb641faa690
aalisiite <47751750+aalisiite@users.noreply.github.com>
2023-03-10 16:40:12 +0700

    Improve tic-tac-toe code:
    - for the current move only, show text instead of a button
    - rewrite Board to use two loops to make the squares instead of hardcoding them.
    - add a toggle button that lets you sort the moves in either ascending or descending order.
    - when someone wins, highlight the three squares that caused the win
    - when no one wins, display a message about the result being a draw.
    - display the location for each move in the format (row, col) in the move history list.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+let winnerLines = [];
+
+function Square({ value, onSquareClick, isWinner }) {
   return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
+    <button className={isWinner ? "winner-square" : "square"} onClick={onSquareClick}> {value} </button>
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay, currentMove }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = 'X';
@@ -26,28 +27,43 @@ function Board({ xIsNext, squares, onPlay }) {
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
+  } else if (currentMove === 9) {
+    status = 'It is a draw!';
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
+  const index = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+  const grid = index.map((i) => {
+    var cells = i.map((j) => {
+      let isWinner = false;
+      if (winnerLines.length !== 0 && winnerLines.includes(j)) {
+        isWinner = true;
+      }
+      return (<Square key={j} value={squares[j]} onSquareClick={() => handleClick(j)} isWinner={isWinner} />);
+    });
+
+    return (
+      <div key={i} className="board-row">{cells}</div>
+    );
+  });
+  winnerLines = [];
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {grid}
+    </>
+  );
+}
+
+function ToggleSwitch({ checked, onChange }) {
+  const switchButton = "Show in ascending order";
+  return (
+    <>
+      <div>{switchButton}</div>
+      <input type="checkbox" id="switch" name='switch' checked={checked} onChange={onChange} />
+      <label htmlFor="switch" className="toggle" />
     </>
   );
 }
@@ -68,27 +84,45 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
-  const moves = history.map((squares, move) => {
+  const [checked, setChecked] = useState(true);
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  const begginingSquares = [null, null, null, null, null, null, null, null, null]
+  let previousHistory = begginingSquares;
+  let moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'Go to move #' + move;
+      const currentHistory = squares;
+      const rowsAndCols = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]];
+      for (let i = 0; i < 9; i++) {
+        if (currentHistory[i] !== previousHistory[i]) {
+          description = <div>{"Row: " + rowsAndCols[i][0] + ", col: " + rowsAndCols[i][1]}</div>;
+          break;
+        }
+      }
+      previousHistory = currentHistory;
     } else {
-      description = 'Go to game start';
+      description = <button onClick={() => jumpTo(move)}>{'Go to game start'}</button>;
     }
+
     return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
+      <li key={move}>{description}</li>
     );
   });
 
+  moves = checked ? moves.sort() : moves.reverse();
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} currentMove={currentMove} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
+      </div>
+      <div className='order'>
+        <ToggleSwitch checked={checked} onChange={handleChange} />
       </div>
     </div>
   );
@@ -108,6 +142,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      winnerLines = lines[i];
       return squares[a];
     }
   }
